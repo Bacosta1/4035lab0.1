712f0cb826b2fe913a01a9f4c4fd909411dcec2d
ICOM STUDENTS <4035b1@mandarina.ece.uprm.edu>
2018-02-26 13:25:51 -0400

    first commit

---
+++ workspace/Lab0/src/Main.java
@@ -0,0 +1,15 @@
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+//public class Main {
+//	public static void main (String[] args){
+//		Scanner sc = new Scanner (System.in);
+//		sc.nextInt();
+//		
+//		ArrayList<Integer>a = new ArrayList<>();
+//		a.add("X"/*5*/); //add X to the end
+//		a.remove("Y"); //remove all Y
+//		 a.get(0); //get
+//		 a.length();// the size of the arraylist
+//	}
+//}
